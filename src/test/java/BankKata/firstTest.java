43eab2d877110dc6c5ae50ea2bc84604d1c796b4
Team NIM <nim.team@bskyb.com>
2017-03-09 15:46:04 +0000

    first commit

---
+++ src/test/java/BankKata/firstTest.java
@@ -0,0 +1,13 @@
+package BankKata;
+
+
+import org.junit.Test;
+
+public class firstTest {
+
+    @Test
+    public void helloWorld() throws Exception {
+        System.out.println("hello");
+
+    }
+}
